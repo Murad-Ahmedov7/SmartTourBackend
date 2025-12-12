a8f1570322100fff7c9ebec3625c59d60ae9d3c4
Samir <Samir@DESKTOP-0V7MBJC>
2025-12-13 02:43:41 +0400

    feat: add User entity and auth DTOs

---
+++ SmartTour.Entities/Users/User.cs
@@ -0,0 +1,30 @@
+﻿
+namespace SmartTour.Entities.Users
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+
+        public required string FullName { get; set; }
+
+        public required string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public bool IsPhoneVerified { get; set; } = false;
+
+        public required string PasswordHash { get; set; }
+
+        public int FailedLoginAttempts { get; set; }
+
+        public DateTime? LockoutUntil { get; set; }
+
+        public DateTime CreatedAt { get; private set; }=DateTime.UtcNow;
+
+        public DateTime? LastLogin { get; set; }
+    }
+
+
+
+}
+
